98c34de061b025805bd1129d9f248f51d04d8678
oricardos <ricardo.rs95@gmail.com>
2025-08-15 17:45:38 -0300

    calculo de distancia

--- js/slide.js
+++ js/slide.js
@@ -2,33 +2,48 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.dist = {
+      finalPosition: 0,
+      startX: 0,
+      movement: 0,
+    };
+  }
+
+  moveSlide(distX) {
+    this.dist.movePosition = distX
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.5;
+    return this.dist.finalPosition - this.dist.movement;
   }
 
   onStart(evt) {
     evt.preventDefault();
+    this.dist.startX = evt.clientX;
     this.wrapper.addEventListener("mousemove", this.onMove);
-
   }
 
-  onMove(evt){
-
+  onMove(evt) {
+    const finalPosition = this.updatePosition(evt.clientX);
+    this.moveSlide(finalPosition)
   }
 
-onEnd(){
+  onEnd() {
     this.wrapper.removeEventListener("mousemove", this.onMove);
-}
+    this.dist.finalPosition = this.dist.movePosition
+  }
 
   addSlideEvents() {
     this.wrapper.addEventListener("mousedown", this.onStart);
     this.wrapper.addEventListener("mouseup", this.onEnd);
-
   }
 
-
-  bindEvents(){
-    this.onStart = this.onStart.bind(this)
-    this.onMove = this.onMove.bind(this)
-    this.onEnd = this.onEnd.bind(this)
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
   }
 
   init() {
